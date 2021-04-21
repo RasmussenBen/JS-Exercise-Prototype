9ed0de6d83c4bf682cefabfa828002ded3bf8a7f
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-04-21 14:05:08 -0500

    completed task 2

--- index.js
+++ index.js
@@ -44,13 +44,18 @@ function Airplane(name) {
    this.age = age;
    this.stomach = [];
   }
- 
- 
+  Person.prototype.eat = function(edible) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(edible);
+    }
+  }
+ Person.prototype.poop = function() {
+   this.stomach = [];
+ }
+ Person.prototype.toString = function() {
+   return `${this.name}, ${this.age}`;
+ }
 
-  
-  
-  
-  
   /*
     TASK 2
       - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -65,10 +70,16 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
-  }
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons;
+  return this.tank;
+}
   
   /*
     TASK 3
